27e6f18c737cb943e2a6a77beb44f7beeb8c6c23
Sergey <joker3839@gmail.com>
2020-07-18 17:56:01 +0300

    1
    1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using MySql.Data.MySqlClient;
+using System.IO;
+using System.Runtime;
 
 namespace TelephoneNumber
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         private static MySqlConnection db;
@@ -26,25 +14,11 @@ namespace TelephoneNumber
         {
             InitializeComponent();
             ShowDB();
-
         }
 
         private void ShowDB() 
         {
-            const string host = "mysql11.hostland.ru";
-            const string database = "host1323541_itstep6";
-            const string port = "3306";
-            const string username = "host1323541_itstep";
-            const string pass = "269f43dc";
-            const string ConnString = "Server=" + host + ";Database=" + database + ";port=" + port + ";User Id=" + username + ";password=" + pass;
-
-            db = new MySqlConnection(ConnString);
-            db.Open();
-
-            string sql = "SELECT name,surname, TelephoneNumber FROM TelephoneBook";
-            var query = new MySqlCommand { Connection = db, CommandText = sql };
-            var result = query.ExecuteReader();
-
+            MySqlDataReader result = ConnectDB("SELECT name, surname, TelephoneNumber FROM TelephoneBook");
             while (result.Read())
             {
                 string Name = result.GetString("name") + " ";
@@ -74,11 +48,26 @@ namespace TelephoneNumber
 
         private void ButtonSearching_Click(object sender, RoutedEventArgs e)
         {
-            ListBoxSearching.Items.Clear();
+            ListBox.Items.Clear();
             string search = BoxSearching.Text;
-
-
             bool messageNoFound  =  false;
+            MySqlDataReader result = ConnectDB($"SELECT * FROM TelephoneBook WHERE name = '{search}' OR surname = '{search}' OR TelephoneNumber = '{search}'");
+            while (result.Read())
+            { 
+                string Name = result.GetString("name") + " ";
+                string Surname = result.GetString("surname") + " ";
+                string TelephoneNumber = result.GetString("TelephoneNumber") + " ";
+                ListBox.Items.Add(Name + Surname + TelephoneNumber);
+                messageNoFound = true;
+            }
+            if (!messageNoFound) 
+            {
+                MessageBox.Show("Нет совпадений по БД", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private MySqlDataReader ConnectDB(string sql) 
+        {
             const string host = "mysql11.hostland.ru";
             const string database = "host1323541_itstep6";
             const string port = "3306";
@@ -87,30 +76,35 @@ namespace TelephoneNumber
             const string ConnString = "Server=" + host + ";Database=" + database + ";port=" + port + ";User Id=" + username + ";password=" + pass;
             db = new MySqlConnection(ConnString);
             db.Open();
-
-            string sql  = $"SELECT * FROM TelephoneBook WHERE name = '{search}' OR surname = '{search}' OR TelephoneNumber = '{search}'";
             var query = new MySqlCommand { Connection = db, CommandText = sql };
             var result = query.ExecuteReader();
-            while (result.Read())
-            { 
-                string Name = result.GetString("name") + " ";
-                string Surname = result.GetString("surname") + " ";
-                string TelephoneNumber = result.GetString("TelephoneNumber") + " ";
-                ListBoxSearching.Items.Add(Name + Surname + TelephoneNumber);
-                messageNoFound = true;
-            }
-
-            if (!messageNoFound) 
-            {
-                MessageBox.Show("Нет совпадений по БД", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
+            return result;
         }
 
-        private void ListBoxSearching_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
         {
-           if( ListBoxSearching.SelectedIndex!=-1)
+            ListBox.Items.Clear();
+            ShowDB();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ListBox.SelectedIndex != -1)
             {
-                RedactName.Text  =  ListBoxSearching.SelectedItem.ToString();
+                //FirstnameInput.Text = ListBox.SelectedItem.ToString();
+                string tempLine = ListBox.SelectedItem.ToString();
+                if (tempLine != null) 
+                {
+                    tempLine = tempLine.Trim();
+                    string[] words = tempLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    /*var index = tempLine.IndexOf(" ");
+                    var lastIndex = tempLine.LastIndexOf(" ");
+                    FirstnameInput.Text = tempLine.Substring(0, index);
+                    SurnameInput.Text = tempLine.Substring(index + 1, lastIndex);
+                    TelephonNamberInput.Text = tempLine.Substring(lastIndex + 1, tempLine.Length - 1); */
+                }
+
+
             }
         }
     }
