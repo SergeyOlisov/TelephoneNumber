4f2b155710d94f091bb445a93522636a41487833
s.olisov <s.olisov@corp.mail.ru>
2020-07-20 23:38:47 +0300

    Update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ namespace TelephoneNumber
     {
         private static MySqlConnection db;
         string id;
+        bool activeAccesButton = false;
+        bool activeChangeButton = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -35,18 +37,38 @@ namespace TelephoneNumber
 
         private void Access_Click(object sender, RoutedEventArgs e)
         {
-            db.Open();
-            string name = FirstnameInput.Text;
-            string surname = SurnameInput.Text;
-            string tel = TelephonNamberInput.Text;
-
-            string sql = $"insert TelephoneBook(name, surname, TelephoneNumber) \n values('{name}','{surname}','{tel}');";
-            var query = new MySqlCommand { Connection = db, CommandText = sql };
-            var result = query.ExecuteNonQuery();
-            MessageBox.Show("Внесенны данные", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Information);
-            db.Close();
-            ListBox.Items.Clear();
-            ShowDB();
+            if(!activeAccesButton)
+            {
+                FirstnameInput.IsEnabled = true;
+                SurnameInput.IsEnabled = true;
+                TelephonNamberInput.IsEnabled = true;
+                activeAccesButton = true;
+            }
+            else
+            {
+                if(FirstnameInput.Text != "" && SurnameInput.Text != "" && TelephonNamberInput.Text != "")
+                {
+                    db.Open();
+                    string name = FirstnameInput.Text;
+                    string surname = SurnameInput.Text;
+                    string tel = TelephonNamberInput.Text;
+                    string sql = $"insert TelephoneBook(name, surname, TelephoneNumber) \n values('{name}','{surname}','{tel}');";
+                    var query = new MySqlCommand { Connection = db, CommandText = sql };
+                    var result = query.ExecuteNonQuery();
+                    MessageBox.Show("Внесенны данные", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Information);
+                    db.Close();
+                    ListBox.Items.Clear();
+                    ShowDB();
+                    FirstnameInput.IsEnabled = false;
+                    SurnameInput.IsEnabled = false;
+                    TelephonNamberInput.IsEnabled = false;
+                    activeAccesButton = false;
+                }
+                else
+                {
+                    MessageBox.Show("Поля не должны быть пустыми, заполните все данные", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
 
         private void ButtonSearching_Click(object sender, RoutedEventArgs e)
@@ -56,17 +78,20 @@ namespace TelephoneNumber
             bool messageNoFound  =  false;
             MySqlDataReader result = ConnectDB($"SELECT * FROM TelephoneBook WHERE name = '{search}' OR surname = '{search}' OR TelephoneNumber = '{search}'");
             while (result.Read())
-            { 
+            {
+                string ID = result.GetInt32("ID").ToString() + " ";
                 string Name = result.GetString("name") + " ";
                 string Surname = result.GetString("surname") + " ";
                 string TelephoneNumber = result.GetString("TelephoneNumber") + " ";
-                ListBox.Items.Add(Name + Surname + TelephoneNumber);
+                ListBox.Items.Add(ID + Name + Surname + TelephoneNumber);
                 messageNoFound = true;
             }
             if (!messageNoFound) 
             {
                 MessageBox.Show("Нет совпадений по БД", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Information);
             }
+            ButtonСhange.IsEnabled = false;
+            Access.IsEnabled = true;
             db.Close();
         }
 
@@ -88,6 +113,11 @@ namespace TelephoneNumber
         private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
         {
             ListBox.Items.Clear();
+            ButtonСhange.IsEnabled = false;
+            Access.IsEnabled = true;
+            FirstnameInput.Clear();
+            SurnameInput.Clear();
+            TelephonNamberInput.Clear();
             ShowDB();
         }
 
@@ -101,40 +131,63 @@ namespace TelephoneNumber
                 {
                     tempLine = tempLine.Trim();
                     words = tempLine.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                    FirstnameInput.Text = words[1];
-                    SurnameInput.Text = words[2];
-                    TelephonNamberInput.Text = words[3];
-                    id = words[0];
+                    if (words.Length < 4)
+                    {
+                        id = words[0];
+                        //TO-DO Надо решить что делать с этим контактом
+                    }
+                    else
+                    {
+                        FirstnameInput.Text = words[1];
+                        SurnameInput.Text = words[2];
+                        TelephonNamberInput.Text = words[3];
+                        id = words[0];
+                    }
                 }
+                ButtonСhange.Visibility = Visibility.Visible;
+                ButtonСhange.IsEnabled = true;
+                Access.IsEnabled = false;
             }
         }
         private void ButtonСhange_Click(object sender, RoutedEventArgs e)
         {
-            int idchanged = Convert.ToInt32(id);
-            const string host = "mysql11.hostland.ru";
-            const string database = "host1323541_itstep6";
-            const string port = "3306";
-            const string username = "host1323541_itstep";
-            const string pass = "269f43dc";
-            const string ConnString = "Server=" + host + ";Database=" + database + ";port=" + port + ";User Id=" + username + ";password=" + pass;
-            db = new MySqlConnection(ConnString);
-            db.Open();
-            string sql = $"UPDATE TelephoneBook SET name = '{FirstnameInput.Text}' , surname = '{SurnameInput.Text}' , TelephoneNumber = '{TelephonNamberInput.Text}'  WHERE ID = {idchanged};";
-            var query = new MySqlCommand { Connection = db, CommandText = sql };
-            var result = query.ExecuteNonQuery();
-            FirstnameInput.Clear();
-            SurnameInput.Clear();
-            TelephonNamberInput.Clear();
-            if (result == 1)
+            if(!activeChangeButton)
             {
-                MessageBox.Show("Данные изменены", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Information);
+                FirstnameInput.IsEnabled = true;
+                SurnameInput.IsEnabled = true;
+                TelephonNamberInput.IsEnabled = true;
+                activeChangeButton = true;
             }
-            else 
+            else
             {
-                MessageBox.Show("Данные не изменены", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Error);
-            } 
-            
-            
+                int idchanged = Convert.ToInt32(id);
+                const string host = "mysql11.hostland.ru";
+                const string database = "host1323541_itstep6";
+                const string port = "3306";
+                const string username = "host1323541_itstep";
+                const string pass = "269f43dc";
+                const string ConnString = "Server=" + host + ";Database=" + database + ";port=" + port + ";User Id=" + username + ";password=" + pass;
+                db = new MySqlConnection(ConnString);
+                db.Open();
+                string sql = $"UPDATE TelephoneBook SET name = '{FirstnameInput.Text}' , surname = '{SurnameInput.Text}' , TelephoneNumber = '{TelephonNamberInput.Text}'  WHERE ID = {idchanged};";
+                var query = new MySqlCommand { Connection = db, CommandText = sql };
+                var result = query.ExecuteNonQuery();
+                FirstnameInput.Clear();
+                SurnameInput.Clear();
+                TelephonNamberInput.Clear();
+                if (result == 1)
+                {
+                    MessageBox.Show("Данные изменены", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Данные не изменены", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                activeChangeButton = false;
+                FirstnameInput.IsEnabled = false;
+                SurnameInput.IsEnabled = false;
+                TelephonNamberInput.IsEnabled = false;
+            }
         }
     }
 }
